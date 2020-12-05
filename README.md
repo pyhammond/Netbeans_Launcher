10d0b0ffeeedee9ad6603f500077c4908779dbd7
Peter Y. Hammond <peter@wfrmls.com>
2020-12-05 15:13:29 -0700

    Initial commit of code

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Netbeans_Launcher
-A shell script and Automator App that Launch Automator with my ssh-agent configuration and connects to our company VPN
+A shell script and Automator App that Launch Automator with my ssh-agent configuration and also connects to our company VPN
+
+## Files
+========
+There are just two files in this repo:
+1. ssh_launcher.sh is the shell script containing the commands built into the Automater App
+2. NetBeans Launcher.app is the Automator app that contains the code in the above shell script.  Feel free to open it in Automator and edit it if necessary
+
+Best of luck !
---
+++ ssh_launcher.sh
@@ -0,0 +1,49 @@
+#!/bin/zsh
+##############################################################################
+##     Configurable Variables                                               ##
+##############################################################################
+# change this first VPN_CONFIG variable to match the _exact_ name of your VPN connection to XMission
+VPN_CONFIG="VPN to XMission"
+
+# The path to your NetBeans App
+NETBEANS="/Applications/NetBeans/Apache NetBeans 12.1.app"
+
+# The location of where we will store our temporary SSH "Environment" script
+SSH_ENV=~/.ssh/environment
+
+##############################################################################
+##     No more things to configure                                          ##
+##############################################################################
+PATH=/bin:/usr/bin:/usr/sbin
+
+# this function launches ssh-agent and adds all of your ssh-keys to the agent if it isn't running
+# It's a singleton so that you don't have tons of ssh keys running all over the place
+function start_ssh_agent {
+	# start the ssh-agent up
+	ssh-agent | sed 's/^echo/#echo/' > "${SSH_ENV}"
+	chmod 600 "${SSH_ENV}"
+	. "${SSH_ENV}" >/dev/null
+
+	# cd into the user's .ssh dir and add all of their keys to the agent
+	pushd ~/.ssh >/dev/null
+	ssh-add -A -K >/dev/null 2>&1
+	popd >/dev/null
+}
+
+# See if the ssh-agent is running and if not, call the function above to start it
+if [ -f "${SSH_ENV}" ]; then
+	. "${SSH_ENV}" >/dev/null
+	ps -p ${SSH_AGENT_PID} | tail -1 | grep ssh-agent$ >/dev/null || {
+		start_ssh_agent
+	}
+else
+	start_ssh_agent;
+fi
+
+# Now, with all of those environment variables set up properly now, we can run NetBeans
+open "${NETBEANS}"
+
+# Lastly, launch VPN if it's not already running
+networksetup -showpppoestatus "${VPN_CONFIG}" | grep ^connected >/dev/null || {
+	networksetup -connectpppoeservice "${VPN_CONFIG}"
+}
